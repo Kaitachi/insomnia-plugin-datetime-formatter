eb97d39a33718afaf89526b1a49717460761dfb6
Radamés <r.alfaro8@icloud.com>
2022-11-05 14:29:45 -0400

    Adding sample Template Tag

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-// For help writing plugins, visit the documentation to get started:
-//   https://docs.insomnia.rest/insomnia/introduction-to-plugins
 
-// TODO: Add plugin code here...
+/**
+* Example template tag that generates a random number
+* between a user-provided MIN and MAX
+*/
+module.exports.templateTags = [{
+    name: 'randomInteger',
+    displayName: 'Random Integer',
+    description: 'Generate a random integer.',
+    args: [
+        {
+            displayName: 'Minimum',
+            description: 'Minimum potential value',
+            type: 'number',
+            defaultValue: 0
+        },
+        {
+            displayName: 'Maximum',
+            description: 'Maximum potential value',
+            type: 'number',
+            defaultValue: 100
+        }
+    ],
+    async run (context, min, max) {
+        return Math.round(min + Math.random() * (max - min));
+    }
+}];
